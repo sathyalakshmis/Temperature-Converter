d7d6f1f93358502b6ee9e29b6294fd5aad699886
Sathyalakshmi S <87848503+sathyalakshmis@users.noreply.github.com>
2022-02-27 19:51:39 +0530

    Update F_to_C.py

--- F_to_C.py
+++ F_to_C.py
@@ -6,16 +6,13 @@ ws.title('°F to °C')
 
 f = ("Times bold", 14)
 
-
 def main():
     ws.destroy()
     import main
 
-
 def C_to_F():
     ws.destroy()
     import C_to_F
-
 
 Label(
     ws,
@@ -27,19 +24,16 @@ Label(
 input = Entry()
 input.pack()
 
-
 def convertFC():
     temp = float(input.get())
     temp_C = (temp-32)*5/9
     ans['text'] = 'Temperature in Celsius is: '+str(int(temp_C))+'°C'
-
 
 button = Button(text='Convert', command=convertFC)
 button.pack()
 
 ans = Label(text=' ', font=('Courrier', 15, 'bold'))
 ans.pack()
-
 
 Button(
     ws,
